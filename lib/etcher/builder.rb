e21a5eafc42a368f1d6e0ffb0521c7471b352b75
Brooke Kuhlmann <brooke@alchemists.io>
2023-03-12 10:26:49 -0600

    Added builder
    The primary work horse for building a configuration from defaults and/or what's in the registry.

---
+++ lib/etcher/builder.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "core"
+require "dry/monads"
+require "refinements/hashes"
+
+module Etcher
+  # Builds a configuration.
+  class Builder
+    include Dry::Monads[:result]
+
+    using Refinements::Hashes
+
+    def initialize registry = Registry.new
+      @registry = registry
+    end
+
+    def call(**overrides)
+      load(overrides).then { |content| transform content }
+                     .bind { |content| validate content }
+                     .bind { |content| record content }
+    end
+
+    private
+
+    attr_reader :registry
+
+    # :reek:NestedIterators
+    # :reek:TooManyStatements
+    def load overrides
+      registry.loaders
+              .map { |loader| loader.call.fmap { |content| content.flatten_keys.symbolize_keys! } }
+              .each
+              .with_object({}) { |content, all| content.bind { |body| all.merge! body } }
+              .merge!(overrides.flatten_keys)
+              .then { |content| Success content }
+    end
+
+    # :reek:NestedIterators
+    def transform content
+      registry.transformers.reduce content do |all, transformer|
+        all.bind { |body| transformer.call body }
+      end
+    end
+
+    def validate content
+      registry.contract
+              .call(content)
+              .to_monad
+              .or { |result| Failure(step: __method__, payload: result.errors.to_h) }
+    end
+
+    def record content
+      Success registry.model[**content.to_h].freeze
+    rescue ArgumentError => error
+      Failure step: __method__, payload: "#{error.message.capitalize}."
+    end
+  end
+end
---
+++ spec/lib/etcher/builder_spec.rb
@@ -0,0 +1,123 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Etcher::Builder do
+  include Dry::Monads[:result]
+
+  subject(:builder) { described_class.new }
+
+  describe "#call" do
+    let(:registry) { Etcher::Registry[contract:, model:] }
+    let(:contract) { Dry::Schema.Params { required(:name).filled :string } }
+    let(:model) { Data.define :name }
+
+    it "answers empty hash with defaults" do
+      expect(builder.call).to eq(Success({}))
+    end
+
+    it "answers frozen hash with defaults" do
+      expect(builder.call.success.frozen?).to be(true)
+    end
+
+    it "answers overrides of defaults" do
+      attributes = {name: "test", label: "Test"}
+      expect(builder.call(**attributes)).to eq(Success(attributes))
+    end
+
+    it "answers record with valid overrides" do
+      expect(builder.call(name: "test")).to eq(Success(name: "test"))
+    end
+
+    it "answers contract failure with invalid overrides" do
+      attributes = {label: "Test"}
+      contract.call attributes
+      builder = described_class.new registry
+
+      expect(builder.call(**attributes)).to eq(
+        Failure(step: :validate, payload: {name: ["is missing"]})
+      )
+    end
+
+    it "answers last loader key with multiple loaders" do
+      registry = Etcher::Registry.new
+      registry.add_loader Etcher::Loaders::JSON.new(SPEC_ROOT.join("support/fixtures/one.json"))
+      registry.add_loader Etcher::Loaders::YAML.new(SPEC_ROOT.join("support/fixtures/two.yaml"))
+      builder = described_class.new registry
+
+      expect(builder.call).to eq(Success(name: "two"))
+    end
+
+    it "answers last nested loader key with multiple loaders" do
+      registry = Etcher::Registry.new
+      registry.add_loader Etcher::Loaders::JSON.new(SPEC_ROOT.join("support/fixtures/three.json"))
+      registry.add_loader Etcher::Loaders::YAML.new(SPEC_ROOT.join("support/fixtures/four.yaml"))
+      builder = described_class.new registry
+
+      expect(builder.call).to eq(Success(test_name: "four"))
+    end
+
+    it "answers mixed keys with multiple loaders" do
+      registry = Etcher::Registry.new
+      registry.add_loader Etcher::Loaders::JSON.new(SPEC_ROOT.join("support/fixtures/one.json"))
+      registry.add_loader Etcher::Loaders::YAML.new(SPEC_ROOT.join("support/fixtures/four.yaml"))
+      builder = described_class.new registry
+
+      expect(builder.call).to eq(Success(name: "one", test_name: "four"))
+    end
+
+    it "answers last nested override key with loaders and overrides" do
+      registry = Etcher::Registry.new
+      registry.add_loader Etcher::Loaders::JSON.new(SPEC_ROOT.join("support/fixtures/three.json"))
+      registry.add_loader Etcher::Loaders::YAML.new(SPEC_ROOT.join("support/fixtures/four.yaml"))
+      builder = described_class.new registry
+      result = builder.call test: {name: "ein"}
+
+      expect(result).to eq(Success({test_name: "ein"}))
+    end
+
+    it "answers record for Data model" do
+      model = Data.define :name
+      registry = Etcher::Registry[model:]
+      builder = described_class.new registry
+
+      expect(builder.call(name: "test")).to eq(Success(model[name: "test"]))
+    end
+
+    it "answers failure with Data argument error" do
+      registry = Etcher::Registry[model: Data.define(:other)]
+      builder = described_class.new registry
+
+      expect(builder.call(name: "test")).to eq(
+        Failure(step: :record, payload: "Unknown keyword: :name.")
+      )
+    end
+
+    it "answers record for Struct model" do
+      model = Struct.new :name
+      registry = Etcher::Registry[model:]
+      builder = described_class.new registry
+
+      expect(builder.call(name: "test")).to eq(Success(model[name: "test"]))
+    end
+
+    it "answers failure with Struct argument error" do
+      registry = Etcher::Registry[model: Struct.new(:other)]
+      builder = described_class.new registry
+
+      expect(builder.call(name: "test")).to eq(
+        Failure(step: :record, payload: "Unknown keywords: name.")
+      )
+    end
+
+    it "answers record for custom contract, model, loaders, transforms, and overrides" do
+      registry = Etcher::Registry[contract:, model:]
+      registry.add_loader Etcher::Loaders::JSON.new(SPEC_ROOT.join("support/fixtures/one.json"))
+      registry.add_transformer(-> content { Success content.merge!(name: content[:name].upcase) })
+      registry.add_transformer(-> content { Success content.merge!(name: "#{content[:name]}!") })
+      builder = described_class.new registry
+
+      expect(builder.call(name: "test")).to eq(Success(model[name: "TEST!"]))
+    end
+  end
+end
