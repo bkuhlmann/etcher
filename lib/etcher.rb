ceb43694258854e8d7500684ea08f6ea31a12c85
Brooke Kuhlmann <brooke@alchemists.io>
2023-09-30 17:11:03 -0600

    Added gem loader
    Provides convenience when inspecting the Zeitwerk loader being used to load this gem.
    
    Milestone: minor

--- lib/etcher.rb
+++ lib/etcher.rb
@@ -4,15 +4,17 @@ require "cogger"
 require "zeitwerk"
 
 Zeitwerk::Loader.new.then do |loader|
+  loader.push_dir __dir__
   loader.inflector.inflect "json" => "JSON", "yaml" => "YAML"
   loader.tag = File.basename __FILE__, ".rb"
-  loader.push_dir __dir__
   loader.setup
 end
 
 # Main namespace.
 module Etcher
   LOGGER = Cogger.new id: :etcher, formatter: :emoji
+
+  def self.loader(registry = Zeitwerk::Registry) = registry.loader_for __FILE__
 
   def self.new(...) = Builder.new(...)
 
