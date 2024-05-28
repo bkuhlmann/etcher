18685525f89f41e47217c633b2d03f8050f39b7f
Brooke Kuhlmann <brooke@alchemists.io>
2024-05-28 17:39:56 -0600

    Added string transformer
    Necessary to format keys with string values using existing attributes as specifiers along with optional auxiliary specifiers if desired.
    
    Milestone: minor

---
+++ lib/etcher/transformers/string.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+
+module Etcher
+  module Transformers
+    # Formats given key using existing and/or ancillary attributes.
+    class String
+      include Dry::Monads[:result]
+
+      def initialize key, **ancillary
+        @key = key
+        @ancillary = ancillary
+      end
+
+      def call attributes
+        value = attributes[key]
+
+        return Success attributes unless value
+
+        Success attributes.merge(key => format(value, **attributes, **ancillary))
+      rescue KeyError => error
+        Failure "Unable to transform #{key.inspect}, missing specifier: " \
+                "\"#{error.message[/<.+>/]}\"."
+      end
+
+      private
+
+      attr_reader :key, :ancillary
+    end
+  end
+end
---
+++ spec/lib/etcher/transformers/string_spec.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Etcher::Transformers::String do
+  include Dry::Monads[:result]
+
+  subject(:transformer) { described_class.new :uri }
+
+  describe "#call" do
+    let :attributes do
+      {
+        organization: "acme",
+        project: "test",
+        uri: "https://test.io/%<organization>s/%<project>s/issues"
+      }
+    end
+
+    it "answers formatted string when key exists" do
+      expect(transformer.call(attributes)).to eq(
+        Success(
+          organization: "acme",
+          project: "test",
+          uri: "https://test.io/acme/test/issues"
+        )
+      )
+    end
+
+    it "answers failure when specifiers are missing" do
+      attributes.delete :project
+
+      expect(transformer.call(attributes)).to eq(
+        Failure(%(Unable to transform :uri, missing specifier: "<project>".))
+      )
+    end
+
+    it "answers original attributes when key doesn't exist" do
+      expect(transformer.call({})).to eq(Success({}))
+    end
+
+    context "with ancillary attributes" do
+      subject(:transformer) { described_class.new :text, tag_a: "A", tag_b: "B" }
+
+      let :attributes do
+        {
+          text: "%<prefix>s: %<message>s [%<tag_a>s %<tag_b>s]",
+          prefix: "Test",
+          message: "A test."
+        }
+      end
+
+      it "answers formatted string" do
+        expect(transformer.call(attributes)).to eq(
+          Success(
+            text: "Test: A test. [A B]",
+            prefix: "Test",
+            message: "A test."
+          )
+        )
+      end
+
+      it "answers formatted string while ignoring ancillary attributes" do
+        attributes[:text] = "%<prefix>s: %<message>s"
+
+        expect(transformer.call(attributes)).to eq(
+          Success(
+            text: "Test: A test.",
+            prefix: "Test",
+            message: "A test."
+          )
+        )
+      end
+    end
+  end
+end
