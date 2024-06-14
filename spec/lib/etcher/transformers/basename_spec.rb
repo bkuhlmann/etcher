cd7f42762e0aeac1eb23e61285ff0b14d07e7c0b
Brooke Kuhlmann <brooke@alchemists.io>
2024-06-14 14:11:42 -0600

    Added basename transformer
    Necessary to tranform value using current directory as a default fallback (with the ability to provide custom fallback if desired).
    
    Milestone: minor

---
+++ spec/lib/etcher/transformers/basename_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Etcher::Transformers::Basename do
+  include Dry::Monads[:result]
+
+  subject(:transformer) { described_class.new :project_name }
+
+  describe "#call" do
+    it "answers name when key exists" do
+      expect(transformer.call({project_name: "test"})).to eq(Success(project_name: "test"))
+    end
+
+    it "answers fallback (default) when key is missing" do
+      transformer = described_class.new :project_name
+      expect(transformer.call({})).to eq(Success({project_name: Pathname.pwd.basename.to_s}))
+    end
+
+    it "answers fallback (custom) when key is missing" do
+      transformer = described_class.new :project_name, fallback: "demo"
+      expect(transformer.call({})).to eq(Success({project_name: "demo"}))
+    end
+  end
+end
