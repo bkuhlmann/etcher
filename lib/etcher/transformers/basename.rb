411ae4999622f32cdf9fc41ba011a003e3b355f2
Brooke Kuhlmann <brooke@alchemists.io>
2024-06-14 14:11:42 -0600

    Added basename transformer
    Necessary to tranform value using current directory as a default fallback (with the ability to provide custom fallback if desired).
    
    Milestone: minor

---
+++ lib/etcher/transformers/basename.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+require "refinements/hash"
+
+module Etcher
+  module Transformers
+    # Conditionally updates value based on path.
+    class Basename
+      include Dry::Monads[:result]
+
+      using Refinements::Hash
+
+      def initialize key, fallback: Pathname.pwd.basename.to_s
+        @key = key
+        @fallback = fallback
+      end
+
+      def call attributes
+        attributes.fetch_value(key) { attributes.merge! key => fallback }
+        Success attributes
+      end
+
+      private
+
+      attr_reader :key, :fallback
+    end
+  end
+end
