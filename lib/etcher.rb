9c5ce4c085a2c24606af8883fcda975b2bd22032
Brooke Kuhlmann <brooke@alchemists.io>
2025-05-21 14:31:51 -0600

    Fixed Zeitwerk 2.7.3 loader failure
    Necessary to address exceptions and spec failures with the recent release of Zeitwerk link:https://github.com/fxn/zeitwerk/blob/main/CHANGELOG.md#273-20-may-2025[2.7.3]. Zeitwerk introduced a breaking change which shouldn't have been a patch release because the `#find` method is no longer available. We have to use `#each` first.
    
    Milestone: patch

--- lib/etcher.rb
+++ lib/etcher.rb
@@ -15,7 +15,7 @@ module Etcher
   LOGGER = Cogger.new id: :etcher
 
   def self.loader registry = Zeitwerk::Registry
-    @loader ||= registry.loaders.find { |loader| loader.tag == File.basename(__FILE__, ".rb") }
+    @loader ||= registry.loaders.each.find { |loader| loader.tag == File.basename(__FILE__, ".rb") }
   end
 
   def self.new(...) = Builder.new(...)
